f512964734144a417257cce8dd4e1a52a1cfb33a
Emir Beltran <jorgeemirbeltran0403@gmail.com>
2025-04-26 23:16:37 -0600

    update index.js to use the new API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const apiRoute = require('./routes/routes');
-
-const app= express();
-
-const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(express.json());
 app.use("/whatsapp", apiRoute);
 
-app.listen(PORT, () => {console.log("el puerto es:" + PORT)});
+module.exports = app; // <- NO hacer app.listen
